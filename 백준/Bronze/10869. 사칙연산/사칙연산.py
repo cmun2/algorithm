f5a497c4048a3132f15d08e1081046b1ed5779b1
cmun2 <cmun2@illinois.edu>
2022-11-03 15:07:28 +0900

    [Bronze V] Title: 사칙연산, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,21 @@
+a,b = map(int, input().split())
+def add():
+    print(a+b)
+        
+def subtract():
+    print(a-b)
+        
+def multiply():
+    print(a*b)
+       
+def divide():
+    print(a//b)
+
+def left():
+    print(a%b)
+        
+add()
+subtract()
+multiply()
+divide()
+left()
